02c2061660dd71c4f4c3d2c5fab4d061e94d7902
Khushi <Km056696@gmail.com>
2026-07-16 23:35:24 +0530

    new

--- app/robots.js
+++ app/robots.js
@@ -1,5 +1,5 @@
 export default function robots() {
-  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+  const siteUrl = (process.env.NEXT_PUBLIC_SITE_URL || "https://luxereva.com").replace(/\/$/, "");
 
   return {
     rules: {
